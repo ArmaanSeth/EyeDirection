df8ad8b799e323ea3f930335f2b1d4cd76deeddd
electroo0 <90555965+amitjimiwal@users.noreply.github.com>
2024-04-28 23:40:02 +0530

    stream video to py websocket serrver

--- server.py
+++ server.py
@@ -1,20 +1,24 @@
 import asyncio
 import websockets
 import cv2
-import mediapipe as mp
 import numpy as np
-import math
-import socket
-# python websocket server
 async def handle_client(websocket, path):
     print("Someone connected")
-    while True:
-        data = await websocket.recv()
-        print(f"Received data: {data}")
-        # Process the received data here
-        # Send a response back to the client
-        response = "Server received your message"
-        await websocket.send(response)
+    try:
+        while True:
+            data = await websocket.recv()
+            nparr = np.frombuffer(data, np.uint8)
+            img_np = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+            cv2.imshow('Received Video', img_np)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+              break
+            # print(f"Received data: {data}")
+            # Process the received data here
+            # Send a response back to the client
+            # response = "Server received your message"
+            # await websocket.send(response)
+    except websockets.exceptions.ConnectionClosed:
+        print("Someone disconnected")
 
 start_server = websockets.serve(handle_client, 'localhost', 8765)
 
