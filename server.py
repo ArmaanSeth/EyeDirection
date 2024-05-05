ad58cc40d4fc4bfedb103a1bc6b526480fdfda08
Armaan Seth <seth.armaanseth@gmail.com>
2024-05-05 15:49:28 +0530

    updated server.py

--- server.py
+++ server.py
@@ -21,6 +21,7 @@ async def handle_client(websocket, path):
 
 
 if __name__ == "__main__":
+    start_server = websockets.serve(handle_client, 'localhost', 8765)
 
     asyncio.get_event_loop().run_until_complete(start_server)
     asyncio.get_event_loop().run_forever()
